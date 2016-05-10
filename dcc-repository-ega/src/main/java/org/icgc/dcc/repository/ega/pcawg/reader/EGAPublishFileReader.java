ad9a7c8aaf7370a90974147b5ae47e6144d2d64b
btiernay <btiernay@oicr.on.ca>
2016-05-10 07:55:34 -0400

    Added stub for EGA PCAWG publish file. Populated some repo ids fields.

---
+++ dcc-repository-ega/src/main/java/org/icgc/dcc/repository/ega/pcawg/reader/EGAPublishFileReader.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2016 The Ontario Institute for Cancer Research. All rights reserved.                             
+ *                                                                                                               
+ * This program and the accompanying materials are made available under the terms of the GNU Public License v3.0.
+ * You should have received a copy of the GNU General Public License along with                                  
+ * this program. If not, see <http://www.gnu.org/licenses/>.                                                     
+ *                                                                                                               
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY                           
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES                          
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT                           
+ * SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,                                
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED                          
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;                               
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER                              
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN                         
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.icgc.dcc.repository.ega.pcawg.reader;
+
+import static org.icgc.dcc.repository.ega.pcawg.model.EGAPublishFile.publishFile;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.icgc.dcc.repository.ega.pcawg.model.EGAPublishFile;
+
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+public class EGAPublishFileReader extends EGAFileReader<EGAPublishFile> {
+
+  /**
+   * Constants.
+   */
+  private static final Pattern PUBLISH_FILE_PATTERN = Pattern.compile(""
+      // Template:
+      // [projectId]/analysis_[type].[study]_[workflow]/TODO.tsv
+      // Example :
+      // LICA-FR/analysis_alignment.PCAWG_WGS_BWA/analysis/TODO.tsv
+      + "([^/]+)" // [projectId]
+      + "/analysis_"
+      + "([^.]+)" // [type]
+      + "\\."
+      + "([^_]+)" // [study]
+      + "_"
+      + "([^/]+)" // [workflow]
+      + "/"
+      + "TODO.tsv");
+
+  public EGAPublishFileReader(File repoDir) {
+    super(repoDir, PUBLISH_FILE_PATTERN);
+  }
+
+  @Override
+  protected ObjectNode readFile(Path path) {
+    // TODO: Parse CSV
+    return null;
+  }
+
+  @Override
+  protected EGAPublishFile createFile(Path path, Matcher matcher) {
+    // val contents = readFile(path);
+
+    // TODO:
+    return publishFile()
+        .build();
+  }
+
+}
